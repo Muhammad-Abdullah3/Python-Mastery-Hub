dd3f285e6d9f57f9dff84a03b1ce56d51b465724
Muhammad Abdullah <maabdullah.rhs@gmail.com>
2023-09-23 22:19:03 +0500

    Add files via upload

---
+++ Python Basics/Chapter 5. Functions/Question_02.py
@@ -0,0 +1,6 @@
+# Creating a Funciton with two arguments and one default argument
+def greet(name,greeting="Hello"):
+    # using f string to greet the person
+    print(f"{greeting} Mr. {name}")
+# Calling the function
+greet("Muhammad","Welcome")
