78d19c9f52c79526b9fda75e5ecafb8a5e37ff18
Muhammad Abdullah <maabdullah.rhs@gmail.com>
2024-02-11 12:20:22 +0500

    Add files via upload

---
+++ Solution/Q2.py
@@ -0,0 +1,5 @@
+# Write a Program that takes more than two inputs in same line and print them on screen.
+x, y, z = input("Enter three integers: ").split(',')
+print("First value entered by user: ",x)
+print("Second value entered by user: ",y)
+print("Three value entered by user: ",z)
